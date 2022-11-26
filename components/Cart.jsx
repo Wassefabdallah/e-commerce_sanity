6cfb6e30d52eb6a39db32bc539aa0967f8f7bc09
Wassefabdallah <wassefabdallah99@gmail.com>
2022-11-26 20:39:58 +0100

    Stripe update

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -2,8 +2,6 @@ import React, { useRef } from 'react';
 import Link from 'next/link';
 import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
 import { TiDeleteOutline } from 'react-icons/ti';
-import toast from 'react-hot-toast';
-
 
 import { useStateContext } from '../context/StateContext';
 import { urlFor } from 'Lib/client';
@@ -13,25 +11,7 @@ const Cart = () => {
   const cartRef = useRef();
   const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuanitity, onRemove } = useStateContext();
 
-  const handleCheckout = async () => {
-    const stripe = await getStripe();
 
-    const response = await fetch('/api/stripe', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(cartItems),
-    });
-
-    if(response.statusCode === 500) return;
-    
-    const data = await response.json();
-
-    toast.loading('Redirecting...');
-
-    stripe.redirectToCheckout({ sessionId: data.id });
-  }
 
   return (
     <div className="cart-wrapper" ref={cartRef}>
