6151f62cc3be529dd595035fdf124ca9f32a69c3
Wassefabdallah <wassefabdallah99@gmail.com>
2022-12-06 22:29:44 +0100

    change title

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 All rights reserverd to MOHAMED WASSEF ABDALLAH</p>
+      <p>2022 All rights reserverd</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>Wassef Online Shop</title>
+        <title>Take-Tech</title>
       </Head>
       <header>
         <Navbar />
