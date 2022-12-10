1985859995fbd62f27bfec21d298fe9c39723863
Wassefabdallah <wassefabdallah99@gmail.com>
2022-12-10 05:48:00 +0100

    component update

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -13,7 +13,7 @@ const Layout = ({ children }) => {
       <header>
         <Navbar />
       </header>
-      <main className="main-container">
+      <main className="main-container" >
         {children}
       </main>
       <footer>
